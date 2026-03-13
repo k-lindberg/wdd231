8de74cf427ecbb15b031ae54ae56afeefda2857b
k-lindberg <kristin.n.lindberg@gmail.com>
2026-03-13 11:05:31 -0600

    week 2 project day 2

---
+++ chamber/scripts/navigation.js
@@ -0,0 +1,9 @@
+// Store the selected elements that we are goint to use.
+const navbutton = document.querySelector('#ham-btn');
+const navlinks = document.querySelector('#nav-bar');
+
+// Toggle the show class off and on
+navbutton.addEventListener('click', () => {
+    navbutton.classList.toggle('show');
+    navlinks.classList.toggle('show');
+});
