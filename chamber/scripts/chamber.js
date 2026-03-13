6f3cb5e7d72a5bbad9dcd147d63f6e234b8f35d9
k-lindberg <kristin.n.lindberg@gmail.com>
2026-03-13 11:30:46 -0600

    week 2 project json update

--- chamber/scripts/chamber.js
+++ chamber/scripts/chamber.js
@@ -1,40 +1,42 @@
-const url = '';
+const url = 'https://raw.githubusercontent.com/k-lindberg/wdd231/main/chamber/data/members.json';
 
 const cards = document.querySelector('#cards');
 
 const displayBusiness = (businesses) => {
-    businesses.forEach((prophet) => {
+    businesses.forEach((business) => {
         let card = document.createElement('section');
-        card.classList.add("prophet-card");
-        let fullName = document.createElement('h2');
-        let portrait = document.createElement('img');
-        let birthDate = document.createElement('p');
-        let birthPlace = document.createElement('p');
+        card.classList.add("business-card");
+        let logo = document.createElement('img');
+        let address = document.createElement('p');
+        let phoneNumber = document.createElement('p');
+        let website = document.createElement('a')
 
-        fullName.textContent = `${prophet.name} ${prophet.lastname}`;
-        birthDate.textContent = `Date of Birth: ${prophet.birthdate}`;
-        birthPlace.textContent = `Place of Birth: ${prophet.birthplace}`;
+        logo.setAttribute('src', business.image);
+        logo.setAttribute('alt', `Logo for ${business.company_name}`);
+        logo.setAttribute('loading', 'lazy');
+        logo.setAttribute('width', '200');
+        logo.setAttribute('height', '100');
 
-        portrait.setAttribute('src', prophet.imageurl);
-        portrait.setAttribute('alt', `Portrait of ${prophet.name} ${prophet.lastname}`);
-        portrait.setAttribute('loading', 'lazy');
-        portrait.setAttribute('width', '340');
-        portrait.setAttribute('height', '440');
+        address.textContent = `${business.address.street}, ${business.address.city_state}`;
+        phoneNumber.textContent = business.phone_number;
 
-        card.appendChild(fullName);
-        card.appendChild(birthDate);
-        card.appendChild(birthPlace);
-        card.appendChild(portrait);
+        website.href = business.website;
+        website.textContent = business.website;
+        website.target = "_blank";
+
+        card.appendChild(logo);
+        card.appendChild(address);
+        card.appendChild(phoneNumber);
+        card.appendChild(website);
 
         cards.appendChild(card);
     });
 }
 
-async function getProphetData() {
+async function getBusinessData() {
     const response = await fetch(url);
     const data = await response.json();
-    //console.table(data.prophets);
-    displayProphets(data.prophets);
+    displayBusiness(data.businesses);
 }
 
-getProphetData();
+getBusinessData();
